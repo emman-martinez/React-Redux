19b6330806a7b50dfa309f61cb65f2a9281bd730
Emmanuel <j.emmanuel.martinez.e@hotmail.com>
2020-02-25 12:28:35 -0600

    Configurando REDUX, Definiendo primeras acciones y Definiendo action y utilizando useDispatch

--- crud_redux/src/components/NuevoProducto.js
+++ crud_redux/src/components/NuevoProducto.js
@@ -1,13 +1,42 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+// ***** Actions de Redux ***** //
+import { crearNuevoProductoAction } from './../actions/productosAction';
 
 const NuevoProducto = () => {
+
+    // Utilizar useDispatch que crea una función 
+    const dispach = useDispatch();
+
+    // Manda a llamar el action "crearNuevoProductoAction" de productosAction
+    const agregarProducto = () => {
+        return (
+            dispach(crearNuevoProductoAction())
+        );
+    };
+
+    // Cuando el usuario haga submit
+    const submitNuevoProducto = (e) => {
+        e.preventDefault();
+
+        // Validar formulario
+
+        // Si no hay errores
+
+        // Crear el nuevo producto
+        agregarProducto();
+        
+    };
+
     return (
         <div className="row justify-content-center">
             <div className="col-md-8">
                 <div className="card">
                     <div className="card-body">
                         <h2 className="text-center mb-4 font-weight-bold">Agregar Nuevo Producto</h2>
-                        <form>
+                        <form
+                            onSubmit={submitNuevoProducto}
+                        >
                             <div className="form-group">
                                 <label>Nombre Producto</label>
                                 <input 
